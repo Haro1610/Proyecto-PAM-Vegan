fa6c3bf1bc71296092877eddbbe909b4850ae219
IñakiOrozco <greenvana14@gmail.com>
2022-03-25 19:27:56 -0600

    Creada toda la estructura básica de archivos de las pantallas
    Ya están las pantallas con el código base de:
    Login (Aaron):
    En esta pantalla el usuario inicia sesión con una cuenta de nuestra app o con una cuenta de google o facebook
    Principal (Aaron):
    En esta pantalla está la receta más votada de la semanal, el apartado de recetas y el apartado de restaurantes además en la parte superior izquierda está un dropdown del perfil qué muestra perfil y cerrar sesión.
    Recetas (Iñaki):
    En esta pantalla se encuentran todas las recetas qué han subido los usuarios como una lista qué se puede hacer scroll y también tiene cada elemento el nombre de la receta y las estrellas (del 1 al 5) qué le han puesto los usuarios.
    Receta en específico (Humberto):
    Al presionar una receta se abre la pantalla de esa receta en específico y contiene el nombre, la calificación de la receta, una foto de esta receta y los ingredientes y el procedimiento, además de el nombre de quien publicó la receta.
    Restaurantes (Humberto):
    Esta pantalla muestra un mapa de los restaurantes qué ofrecen opciones veganas/vegetarianas en la ciudad y se puede ampliar y recorrer.
    Perfil (Iñaki):
    Al presionar en el perfil se muestra una pantalla con la foto del perfil, el nombre y cuantas recetas ha publicado

---
+++ lib/recetaEsp/recetaEsp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecetaEsp extends StatefulWidget {
+  RecetaEsp({Key? key}) : super(key: key);
+
+  @override
+  State<RecetaEsp> createState() => _RecetaEspState();
+}
+
+class _RecetaEspState extends State<RecetaEsp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
