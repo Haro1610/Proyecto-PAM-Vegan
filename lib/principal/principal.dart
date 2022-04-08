7e79739c1d6665eae91a007dc231e1819a98f743
IñakiOrozco <greenvana14@gmail.com>
2022-04-07 23:52:36 -0500

    location

--- lib/principal/principal.dart
+++ lib/principal/principal.dart
@@ -149,13 +149,13 @@ class _PrincipalState extends State<Principal> {
 }
 
 void _checkPermissions() async {
-  var status = await Permission.camera.status;
+  var status = await Permission.location.status;
   if (status.isDenied) {
-    // We didn't ask for permission yet or the permission has been denied before but not permanently.
+    print("Denied");
   }
 
 // You can can also directly ask the permission about its status.
   if (await Permission.location.isRestricted) {
-    // The OS restricts access, for example because of parental controls.
+    print("Restricted");
   }
 }
