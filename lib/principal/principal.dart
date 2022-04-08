3fbf27e4937c76038db375f6ae862c6687fa5a17
humbertpa <humbertpal@gmail.com>
2022-04-07 20:53:23 -0500

    Se elimino la appbar para mayor espacio y se coloco un drawer con los mismos recursos.

--- lib/principal/principal.dart
+++ lib/principal/principal.dart
@@ -19,93 +19,129 @@ class _PrincipalState extends State<Principal> {
   final _pagesNameList = ["Inicio", "Restaurantes", "Recetas", "Recetas esp"];
   final _pagesList = [
     Center(child: Text("Inicio")),
-    Center(child: Text("Restaurantes")),
-    Center(child: Text("Recetas")),
+    MapSample(),
+    Recetas(),
     Center(child: Text("Recetas esp"))
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Inicio"),
-          actions: <Widget>[
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => Perfil()));
-                  },
-                  icon: Icon(Icons.account_circle_rounded),
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => Login()));
-                  },
-                  icon: Icon(Icons.logout),
-                ),
-              ],
-            )
-          ],
-          flexibleSpace: Container(
-              height: 120,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Colors.green,
-                Color.fromARGB(255, 152, 202, 36)
-              ]))),
+      /*      appBar: AppBar(
+        title: const Text("Inicio"),
+        actions: <Widget>[
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Perfil()));
+                },
+                icon: Icon(Icons.account_circle_rounded),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Login()));
+                },
+                icon: Icon(Icons.logout),
+              ),
+            ],
+          )
+        ],
+        flexibleSpace: Container(
+          height: 120,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.green, Color.fromARGB(255, 152, 202, 36)],
+            ),
+          ),
         ),
-        body: Stack(
+      ), */
+      /*body:  Stack(
+        children: [
+          Card(
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Image.network(
+                "https://cdn.kiwilimon.com/recetaimagen/27982/th5-640x640-27850.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ), */
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
           children: [
-            Card(
-              child: Column(mainAxisSize: MainAxisSize.max, children: [
-                AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: Image.network(
-                    "https://cdn.kiwilimon.com/recetaimagen/27982/th5-640x640-27850.jpg",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ]),
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
             ),
-            IndexedStack(
-              index: _currentPageIndex,
-              children: _pagesList,
+            ListTile(
+              leading: const Icon(Icons.account_circle_rounded),
+              title: Text("Mi perfil"),
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Perfil()));
+              },
             ),
+
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: Text("Cerrar Sesion"),
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Login()));
+              },
+            ),
+
+            // Update the state of the app.
+            // ...
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _currentPageIndex,
-          onTap: (index) {
-            setState(() {
+      ),
+      body: IndexedStack(
+        index: _currentPageIndex,
+        children: _pagesList,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentPageIndex,
+        onTap: (index) {
+          setState(
+            () {
               _currentPageIndex = index;
-              if (_currentPageIndex == 2) {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Recetas()));
-              } else if (_currentPageIndex == 1) {
+              if (_currentPageIndex == 1) {
                 Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) => MapSample()));
               }
-            });
-            // Respond to item press.
-          },
-          items: [
-            BottomNavigationBarItem(
-              label: _pagesNameList[0],
-              icon: Icon(Icons.home_filled),
-            ),
-            BottomNavigationBarItem(
-              label: _pagesNameList[1],
-              icon: Icon(Icons.map_outlined),
-            ),
-            BottomNavigationBarItem(
-              label: _pagesNameList[2],
-              icon: Icon(Icons.fastfood_outlined),
-            ),
-          ],
-        ));
+            },
+          );
+          // Respond to item press.
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: _pagesNameList[0],
+            icon: Icon(Icons.home_filled),
+          ),
+          BottomNavigationBarItem(
+            label: _pagesNameList[1],
+            icon: Icon(Icons.map_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: _pagesNameList[2],
+            icon: Icon(Icons.fastfood_outlined),
+          ),
+        ],
+      ),
+    );
   }
 }
