65befeaf4a4ed3c14b967b210796c51223183c10
IñakiOrozco <greenvana14@gmail.com>
2022-05-02 00:44:00 -0500

    todo al cien pa q transa

--- lib/recetas/recetas.dart
+++ lib/recetas/recetas.dart
@@ -86,8 +86,6 @@ class Platillo extends StatelessWidget {
 }
 
 class Recetas extends StatelessWidget {
-  final _decoracion =
-      BoxDecoration(border: Border.all(color: Colors.blueAccent));
   TextEditingController _titulo = new TextEditingController();
   TextEditingController _ingredientes = new TextEditingController();
   TextEditingController _procedimiento = new TextEditingController();
@@ -172,7 +170,16 @@ class Recetas extends StatelessWidget {
                 context: context,
                 barrierDismissible: true,
                 builder: (_) => AlertDialog(
-                  title: Text("Nueva receta"),
+                  title: Text(
+                    "Nueva receta",
+                    style: GoogleFonts.pacifico(
+                      textStyle: TextStyle(
+                        fontSize: 30.0,
+                        color: Color.fromARGB(255, 17, 88, 19),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                   content: Wrap(
                     runSpacing: 18,
                     spacing: 18,
@@ -291,7 +298,7 @@ class Recetas extends StatelessWidget {
           ),
           Expanded(
             child: Container(
-              decoration: _decoracion,
+              decoration: null,
               height: 0,
               child: ListView(
                 scrollDirection: Axis.vertical,
