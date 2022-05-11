1a3fdb24a79a5bbced545f8fb9258f602598f9ca
IñakiOrozco <greenvana14@gmail.com>
2022-05-11 11:11:06 -0500

    fixed map

--- lib/restaurantes/restaurantes.dart
+++ lib/restaurantes/restaurantes.dart
@@ -20,32 +20,66 @@ class MapSampleState extends State<MapSample> {
 
   @override
   Widget build(BuildContext context) {
+    final _mapScreen = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.lightGreen,
-              Colors.blueAccent,
-            ],
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://9to5google.com/2017/04/27/google-maps-android-home-screen-india/google-maps-india-1/"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
+      body: GoogleMap(
+        initialCameraPosition: initialMapPosition,
+        markers: markers,
+        zoomControlsEnabled: false,
+        mapType: MapType.normal,
+        onMapCreated: (GoogleMapController controller) {
+          googleMapController = controller;
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          Position position = await _determinePosition();
+
+          googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(position.latitude, position.longitude),
+                  zoom: 14)));
+
+          markers.clear();
+
+          markers.add(Marker(
+              markerId: const MarkerId('Ubicacion'),
+              position: LatLng(position.latitude, position.longitude)));
+
+          setState(() {});
+        },
+        label: const Text("Tu ubicacion"),
+        icon: const Icon(Icons.location_history),
       ),
     );
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission userPermission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    userPermission = await Geolocator.checkPermission();
+
+    if (userPermission == LocationPermission.denied) {
+      userPermission = await Geolocator.requestPermission();
+
+      if (userPermission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
+      }
+    }
+
+    if (userPermission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied');
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    return position;
   }
 }
