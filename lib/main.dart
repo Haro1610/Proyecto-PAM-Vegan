0169c139cb7a17fc5fe01d31ef50d397bb42f94a
humbertpa <humbertpal@gmail.com>
2022-05-10 22:34:25 -0500

    fsfsdfsdf

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:planternativo/auth/bloc/auth_bloc.dart';
 import 'package:planternativo/login/login.dart';
 import 'package:planternativo/principal/principal.dart';
-<<<<<<< HEAD
-import 'package:planternativo/recetas/bloc/crear_bloc.dart';
-=======
 import 'package:planternativo/recetaEsp/recetaEsp.dart';
+import 'package:planternativo/recetas/bloc/crear_bloc.dart';
 import 'package:planternativo/restaurantes/restaurantes.dart';
 import 'package:planternativo/perfil/perfil.dart';
->>>>>>> main
 
 //Bloc
 
@@ -36,23 +33,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
       title: 'Material App',
-<<<<<<< HEAD
-      home: BlocConsumer<AuthBloc, AuthState>(
-        listener: (context, state) {
-          // TODO: implement listener
-        },
-        builder: (context, state) {
-          if (state is AuthSuccesState) {
-            return Principal();
-          } else if (state is AuthErrorState) {
-            return Login();
-          } else if (state is SingOutSucces) {
-            return Login();
-          }
-          return Center(child: CircularProgressIndicator());
-        },
-      ),
-=======
       home: Restaurantes(),
       /*MultiBlocProvider(
         providers: [
@@ -63,7 +43,6 @@ class MyApp extends StatelessWidget {
         ],
         child: Principal(),
       ),*/
->>>>>>> main
     );
   }
 }
