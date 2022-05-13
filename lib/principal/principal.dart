7da0eb6c2dfe9ad6edff61113a9777e633be7926
IñakiOrozco <greenvana14@gmail.com>
2022-05-13 02:24:43 -0500

    Ya funciona la receta del día

--- lib/principal/principal.dart
+++ lib/principal/principal.dart
@@ -12,6 +12,7 @@ import 'package:planternativo/recetas/recetas.dart';
 import 'package:planternativo/restaurantes/restaurantes.dart';
 import 'package:planternativo/restaurantes/restaurantes.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'dart:math';
 
 class Principal extends StatefulWidget {
   Principal({Key? key}) : super(key: key);
@@ -26,6 +27,7 @@ class _PrincipalState extends State<Principal> {
   @override
   Widget build(BuildContext context) {
     final _screen = MediaQuery.of(context).size;
+
     String _telefono = "3318503861";
     String _imagen =
         "https://www.sabrosia.pr/resizer/U8dw60E4ucVskb2IH5vRcWVWlvw=/1440x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/JDPCVRO6AFGATNQHEVA4EFZCTA.jpg";
@@ -233,75 +235,61 @@ class _PrincipalState extends State<Principal> {
                     fontWeight: FontWeight.bold,
                   )),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    //NECESITAMOS TENER UNA RECETA SIN HARDCODEAR PENDEJOS ESTÚPIDOS
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => RecetasEsp({
-                              "stars": 5,
-                              "name": "Ceviche de seitán",
-                              "author": "Iñaki Orozco",
-                              "ingredients":
-                                  "1/4 de Choclo desgranando\n2 Camotes amarillos\n1/4 de Seitán natural\n200g de champiñones frescos\n1 Pimiento\n3/4  de cebolla\n5 tallos de apio\n200ml de jugo de Kión\n1 ají limo\nZumo de 8 limones",
-                              "image":
-                                  "https://scontent-qro1-1.xx.fbcdn.net/v/t1.6435-9/118733503_3271728659586818_2944357760166773043_n.jpg?stp=dst-jpg_p180x540&_nc_cat=100&ccb=1-6&_nc_sid=8bfeb9&_nc_eui2=AeGEsAERyoGeDkBU8gK1RSLIInHNmF2cwJAicc2YXZzAkPAXYw4-tnfoJdcm0BVRPJuqcx0vQjrIRLAuQaO72XHT&_nc_ohc=XilYn5eTPQIAX8KCLBQ&_nc_ht=scontent-qro1-1.xx&oh=00_AT_WHZdrr4WWBekCN4FWPfJrZ-dYjzIqug2OOtQm3zfe8A&oe=62A217DB",
-                              "description":
-                                  "Sancochar los Camotes\nSancochar el Choclo\nCortar en cuadritos el Seitán y los champiñones en láminas.\nCortar la cebolla tipo pluma. \nEl Pimiento cortar en cuadraditos.\n Licuar el apio, ají limo( a gusto), limón, sal.\nEn un bold mezclar el Seitán natural con los champiñones y el Pimiento. Incorporar lo licuado.\nDejar incurrir por 3 minutos.\nServir sobre una hoja de Lechuga, el Ceviche, acompañar con el Camote cortado en rodajas, con choclo desgranando, y la cebolla  fresca."
-                            })));
-                  },
-                  child: Container(
-                    height: 200,
-                    width: 200,
-                    child: Card(
-                      semanticContainer: true,
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      child: BlocConsumer<PendingBloc, PendingState>(
-                          listener: (context, state) {},
-                          builder: (context, state) {
-                            if (state is PendingFotosSuccessState) {
-                              return ListView.builder(
-                                itemCount: state.myData.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  //una vez que acabe el ciclo, ya tendremos el elemento con más calificación, tenemos que regresarlo
-                                  int acum = 0;
-                                  if (state.myData[index]['stars'] > acum) {
-                                    acum = state.myData[index]['stars'];
-                                  }
-                                  var platillo = {
-                                    state.myData[index]['stars'],
-                                    state.myData[index]['nombre'],
-                                    state.myData[index]['autor'],
-                                    state.myData[index]['ingredientes'],
-                                    state.myData[index]['imagen'],
-                                    state.myData[index]['procedimiento']
-                                  };
-                                  return Image.network(
-                                    state.myData[index]['imagen'],
-                                    fit: BoxFit.fill,
-                                  );
-                                },
-                              ); /*
-                              itemBuilder:
-                              (BuildContext context, int index) {
-                                return Image.network(
-                                  state.myData[index]['imagen'],
-                                  fit: BoxFit.fill,
-                                );
-                              };*/
-                            }
-                            return Image.network(
-                              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feskipaper.com%2Fimages%2Fgreen-13.jpg&f=1&nofb=1",
-                              fit: BoxFit.fill,
-                            );
-                          }),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      elevation: 5,
-                      margin: EdgeInsets.all(10),
-                    ),
-                  ),
-                ),
+                BlocConsumer<PendingBloc, PendingState>(
+                    listener: (context, state) {},
+                    builder: (context, state) {
+                      if (state is PendingFotosSuccessState) {
+                        int index = Random().nextInt(state.myData.length);
+                        return GestureDetector(
+                          onTap: () {
+                            //NECESITAMOS TENER UNA RECETA SIN HARDCODEAR PENDEJOS ESTÚPIDOS
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => RecetasEsp({
+                                      "stars": state.myData[index]['stars'],
+                                      "name": state.myData[index]['nombre'],
+                                      "author": state.myData[index]['autor'],
+                                      "ingredients": state.myData[index]
+                                          ['ingredientes'],
+                                      "image": state.myData[index]['imagen'],
+                                      "description": state.myData[index]
+                                          ['procedimiento']
+                                    })));
+                          },
+                          child: Container(
+                            height: 200,
+                            width: 200,
+                            child: Card(
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              child: Image.network(
+                                state.myData[index]['imagen'],
+                                fit: BoxFit.fill,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              elevation: 6,
+                              margin: EdgeInsets.all(10),
+                            ),
+                          ),
+                        );
+                      }
+                      return Container(
+                        height: 200,
+                        width: 200,
+                        child: Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: Image.network(
+                            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feskipaper.com%2Fimages%2Fgreen-13.jpg&f=1&nofb=1",
+                            fit: BoxFit.fill,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 6,
+                          margin: EdgeInsets.all(10),
+                        ),
+                      );
+                    }),
               ],
             )
           ]),
